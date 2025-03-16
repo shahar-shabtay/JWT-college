fe331fa13c15d74503ee8aaca1927adfeed32897
Yuval <yuval@example.com>
2025-03-17 01:48:54 +0200

    work

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 const envFile = process.env.ENV_FILE || '.env';
 console.log(`Using environment file: ${envFile}`);
@@ -24,7 +25,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Allow CORS from our client
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',  // For local development
+      'https://node71.cs.colman.ac.il',  // For production
+      'https://193.106.55.231',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
@@ -51,9 +56,17 @@ app.use('/auth', authRoutes);
 import likeRoute from './routes/likes'
 app.use('/likes', likeRoute);
 
-
 import fileRoute from './routes/file';
 app.use('/file', fileRoute);
 app.use("/public", express.static('public'));
+
+// Serve front
+const frontPath = path.join(__dirname, "front");
+app.use(express.static(frontPath));
+
+// Fallback to client routing
+app.get("*", (req,res) => {
+  res.sendFile(path.join(frontPath, "index.html"));
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import app from './app';
 import dotenv from 'dotenv';
 import swaggerUi from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 const envFile = process.env.ENV_FILE || '.env';
 dotenv.config({ path: envFile });
@@ -10,7 +13,7 @@ dotenv.config({ path: envFile });
 const port: string = process.env.PORT || "3000";
 
 // Swagger setup
-const options = {
+const swagger_options = {
   definition: {
     openapi: "3.0.0",
     info: {
@@ -18,15 +21,21 @@ const options = {
       version: "1.0.0",
       description: "REST server including authentication using JWT and Refresh token",
     },
-    servers: [{ url: "http://localhost:3000" }],
+    servers: [{ url: "https://node71.cs.colman.ac.il" }],
   },
   apis: ["./src/routes/*.ts"], 
 };
 
-const specs = swaggerJsDoc(options);
+const specs = swaggerJsDoc(swagger_options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+if(process.env.NODE_ENV !== 'production') {
+  console.log('development mode');
+  http.createServer(app).listen(port)
+} else {
+  const options = {
+    key: fs.readFileSync('./client-key.pem'),
+    cert: fs.readFileSync('./client-cert.pem')
+  }
+  https.createServer(options, app).listen(port)
+}
