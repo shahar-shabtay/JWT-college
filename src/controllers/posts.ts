6e19105966f4da153da21b8fa9954a14127d36fd
shahar-shabtay <127850433+shahar-shabtay@users.noreply.github.com>
2025-03-17 20:50:28 +0200

    Revert "working paging"

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -9,19 +9,21 @@ class postController extends BaseController<IPost> {
     }
 
     // Create new object
-    async post(req: Request, res: Response) {
+    async post (req: Request, res: Response) {
         const userId = req['user']._id;
         const message = req.body;
-        message.owner = userId;
+        message.owner = userId
         super.post(req, res);
-    }  
+    }
+
 }
 
 const getPostsByOwner = async (req: Request, res: Response) => {
     try {
-        const ownerId = req.query.owner;
+        const ownerId = req.query.owner;  // Get the owner ID from query params
+
         if (!ownerId) {
-            return res.status(400).json({ error: 'Owner ID is required' });
+            return res.status(400).json({ error: 'Owner ID is required' });  // Return an error if owner ID is missing
         }
 
         // Find posts by owner ID and populate owner details
@@ -40,35 +42,5 @@ const getPostsByOwner = async (req: Request, res: Response) => {
     }
 };
 
-// Get All Posts with Pagination
-const getAllWithPaging = async (req: Request, res: Response) => {
-    try {
-        // Ensure page and limit are valid numbers
-        const pageNum = Number(req.query.page) || 1;
-        const limitNum = Number(req.query.limit) || 10;
-
-        if (pageNum < 1 || limitNum < 1) {
-            return res.status(400).json({ message: "Page and limit must be positive numbers." });
-        }
-
-        const posts = await Post
-            .find()
-            .skip((pageNum - 1) * limitNum)
-            .limit(limitNum);
-
-        const totalCount = await Post.countDocuments();
-
-        res.json({
-            data: posts,
-            totalPages: Math.ceil(totalCount / limitNum),
-            currentPage: pageNum,
-            totalItems: totalCount,
-        });
-    } catch (error) {
-        console.error("Error fetching posts:", error);
-        res.status(500).json({ message: "Server error" });
-    }
-}  
-
 export default new postController(Post);
-export { getPostsByOwner , getAllWithPaging };
+export { getPostsByOwner };
